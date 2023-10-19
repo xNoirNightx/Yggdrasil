4735e5efdd12fd3c902371234d0c89657c697370
Coral <cnight18@hotmail.com>
2023-10-19 13:28:04 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Asgard from './Pages/Asgard';
-import Midgard from './Pages/Midgard';
-import Vanaheim from './Pages/Vanaheim';
-import Jotunheim from './Pages/Jotunheim';
-import Alfheim from './Pages/Alfheim';
-import Svartalfheim from './Pages/Svartalfheim';
-import Niflheim from './Pages/Niflheim';
-import Muspelheim from './Pages/Muspelheim';
-import Helheim from './Pages/Helheim';
+import Yggdrasil from './Yggdrasil';
+import Asgard from './Component/Asgard';
+import Midgard from './Component/Midgard';
+import Vanaheim from './Component/Vanaheim';
+import Jotunheim from './Component/Jotunheim';
+import Alfheim from './Component/Alfheim';
+import Svartalfheim from './Component/Svartalfheim';
+import Niflheim from './Component/Niflheim';
+import Muspelheim from './Component/Muspelheim';
+import Helheim from './Component/Helheim';
 // import Button from './Button';
 
 function App() {
   return (
     <Router>
       <Switch>
+       <Route path="/src/Yggdrasil.js" exact component={Yggdrasil} />
         <Route path="/asgard" component={Asgard} />
         <Route path="/midgard" component={Midgard} />
         <Route path="/vanaheim" component={Vanaheim} />

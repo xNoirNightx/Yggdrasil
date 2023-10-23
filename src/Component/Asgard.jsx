422b676649334d144b6a0130d27030d1edc68bc9
Coral <cnight18@hotmail.com>
2023-10-22 21:22:26 -0300

    page updates

--- src/Component/Asgard.jsx
+++ src/Component/Asgard.jsx
@@ -1,11 +1,242 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Asgard() {
+  const containerStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+  };
+
+  const headerStyle = {
+    marginBottom: '20px',
+  };
+
+  const imageStyle = {
+    width: '50%',
+  };
+
+  const smallerImageStyle = {
+    width: '30%',
+    margin: '10px',
+    cursor: 'pointer',
+  };
+
+  const leftImageStyle = {
+    flex: 1,
+    cursor: 'pointer',
+  };
+
+  const rightImageStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  };
+
+  const [leftImageTextVisible, setLeftImageTextVisible] = useState(false);
+  const [rightImageTextVisible, setRightImageTextVisible] = useState(false);
+
+  const [image1TextVisible, setImage1TextVisible] = useState(false);
+  const [image2TextVisible, setImage2TextVisible] = useState(false);
+  const [image3TextVisible, setImage3TextVisible] = useState(false);
+
+  const toggleLeftImageText = () => {
+    setLeftImageTextVisible(!leftImageTextVisible);
+    setRightImageTextVisible(false);
+  };
+
+  const toggleRightImageText = () => {
+    setRightImageTextVisible(!rightImageTextVisible);
+    setLeftImageTextVisible(false);
+  };
+
+  const toggleImage1Text = () => {
+    setImage1TextVisible(!image1TextVisible);
+    setImage2TextVisible(false);
+    setImage3TextVisible(false);
+  };
+
+  const toggleImage2Text = () => {
+    setImage2TextVisible(!image2TextVisible);
+    setImage1TextVisible (false);
+    setImage3TextVisible(false);
+  };
+
+  const toggleImage3Text = () => {
+    setImage3TextVisible(!image3TextVisible);
+    setImage1TextVisible(false);
+    setImage2TextVisible(false);
+  };
+
+  const imagesContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
   return (
-    <div>
-      <h1>Welcome to Asgard</h1>
-      <p>This is the realm of the Aesir and powerful gods.</p>
-      {/* Add content and functionality specific to Asgard here */}
+    <div style={containerStyle}>
+      <div style={headerStyle}>
+        <h1>Welcome to Asgard</h1>
+        <p>This is the realm of the Aesir and powerful gods.</p>
+      </div>
+
+      <div style={imagesContainerStyle}>
+        <div style={leftImageStyle} onClick={toggleLeftImageText}>
+          <img
+            src={`${process.env.PUBLIC_URL}/realms/AsgardBG.png`}
+            alt="Left Image"
+            style={imageStyle}
+          />
+          {leftImageTextVisible && (
+            <p>
+              Asgard, the celestial realm of Norse mythology, is the splendid
+              home of the Aesir, a primary group of divine beings. This
+              magnificent city stands high in the heavens, accessible only
+              through the ethereal Bifrost, a rainbow bridge. The city's
+              grandeur is protected by a mighty wall, a creation attributed to
+              Odin and his brothers, Vili and Ve. Asgard features numerous
+              significant halls, including Valhalla, where brave warriors find
+              their place in Odin's Hall of the Fallen. Vingolf is the majestic
+              hall of the goddesses, while Alfheim belongs to Freyr, the god of
+              abundance. In addition to these halls, Asgard houses landmarks
+              like Mimir's Well, a source of wisdom guarded by the wise giant
+              Mimir, and Heimdall's golden hall, the sentinel of the Bifrost
+              bridge. Asgard is home to prominent gods and goddesses, such as
+              Thor, Loki, Baldur, and Tyr, who play pivotal roles in shaping the
+              destinies of both the divine and mortal worlds. This celestial
+              realm is a place of splendour and a stage for conflicts and drama.
+              Gods and goddesses face personal challenges and engage in epic
+              battles against giants, monsters, and formidable adversaries.
+              Asgard is the central and splendid realm within the Norse cosmos,
+              home to the Aesir gods. Linked to the realm of humans through the
+              Bifrost bridge, Asgard is the backdrop for countless mythological
+              tales found in revered literature. These tales reveal the lives,
+              exploits, and relationships of the gods and goddesses, reflecting
+              the core tenets of Norse mythology. As a symbol of divine power
+              and order, Asgard showcases celestial grandeur amidst cosmic
+              turbulence. Like Valhalla and Gladsheim, its halls exemplify Norse
+              ideals of bravery, glory, wisdom, and leadership. The stories of
+              Asgard continue to inspire and captivate, preserving the rich
+              cultural heritage of the Norse people.
+            </p>
+          )}
+        </div>
+        <div style={rightImageStyle} onClick={toggleRightImageText}>
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Odin.png`}
+            alt="Image 1"
+            style={smallerImageStyle}
+            onClick={toggleImage1Text}
+          />
+          {image1TextVisible && (
+            <p>
+              Odin plays a crucial role in Norse mythology, commanding the
+              hierarchy of Norse deities. As the supreme god, Odin rules over
+              Asgard, the divine realm of the gods. He is often described as a
+              figure of profound wisdom and formidable power, associated with
+              domains that include knowledge, warfare, poetry, and sorcery. Odin
+              is known by many titles, with "Allfather" being one of the most
+              prominent, as he is considered the father of the gods in Norse
+              mythology. A defining trait of Odin is his unrelenting search for
+              knowledge and wisdom. A remarkable example of this pursuit is the
+              self-sacrifice of his right eye to drink from the Well of Wisdom,
+              granting him phenomenal knowledge and profound insight. Odin's
+              thirst for knowledge also extends to studying runes, mysterious
+              symbols representing power and communication. He unlocked their
+              secrets through a self-imposed trial of hanging himself from the
+              World Tree, Yggdrasil. Odin assumes the role of a war god, often
+              portrayed as a fierce and strategic leader of the divine forces in
+              battle. He embodies qualities such as bravery, cunning, and
+              tactical warfare. Odin is served by the Valkyries, a group of
+              female warriors who act as his warriors who choose fallen soldiers
+              from past wars to be escorted to Valhalla, the grand hall where
+              the honoured killed are housed.
+            </p>
+          )}
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Thor.png`}
+            alt="Image 2"
+            style={smallerImageStyle}
+            onClick={toggleImage2Text}
+          />
+          {image2TextVisible && (
+            <p>
+              Thor, a central and widely recognized God in Norse mythology, is
+              recognized as one of the most prominent deities within the
+              pantheon. Born of the union between Odin and Fjörgyn, Thor is
+              frequently depicted as a challenging and mighty warrior. His
+              dominion contains thunder, lightning, sheer physical strength, and
+              the guardianship of divine realms and humanity. At the heart of
+              Thor's persona is his iconic weapon, Mjolnir, a hammer only usable
+              to those worthy; it grants him control over the weather and the
+              ability to deliver devastating blows to his enemies. As a guardian
+              of Asgard, the divine realm, and Midgard, the human world, Thor
+              represents bravery, valour, and unwavering loyalty. He is often
+              portrayed as an approachable and amiable deity, ever prepared to
+              engage in conflicts to protect divine beings and mortals from
+              various threats, particularly those posed by giants. Among Thor's
+              legendary exploits, his battles against the giants perennial
+              threats to gods and mankind, are mainly celebrated. His strength,
+              allowing him to lift unimaginably heavy objects and confront
+              daunting attackers, is a source of great reverence. His adventures
+              are marked by challenges that he overcomes through a combination
+              of extraordinary physical power, strategic insight, and the
+              assistance of Mjolnir. Thor's vivacious and impulsive nature is
+              balanced by his bravery and devotion to justice, making him an
+              endearing and revered figure in Norse mythology. As the protector
+              of both gods and mortals, Thor represents strength, guardianship,
+              and the ultimate triumph of good over evil. His enduring
+              popularity and importance in Norse mythology firmly establish him
+              as one of the pantheon's most iconic and influential figures.
+              Thor's stories, filled with epic battles, magical weaponry, and
+              divine justice, continue to inspire works of literature, art, and
+              modern pop culture. As the god of thunder and the defender of
+              realms, Thor's character resonates with timeless ideals of
+              courage, loyalty, and the unwavering pursuit of righteousness,
+              leaving a lasting impact on audiences worldwide.
+            </p>
+          )}
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Loki.png`}
+            alt="Image 3"
+            style={smallerImageStyle}
+            onClick={toggleImage3Text}
+          />
+          {image3TextVisible && (
+            <p>
+              Loki, a complicated character in Norse mythology, is one of the
+              most well-known gods in the pantheon. He's famous for being a
+              shape-shifter and a master trickster. Loki is part of the Aesir,
+              the leading group of gods in Norse mythology. He's known for being
+              clever, mischievous, and unpredictable, and his actions can lead
+              to good and bad outcomes for the gods and the broader world of
+              Norse mythology. Even though Loki wasn't born among the Aesir, he
+              has a strong connection with them. He's considered a blood-brother
+              to Odin, the All-Father of the gods. Loki is often portrayed as a
+              provocative and sly figure. He's brilliant and can shapeshift into
+              anything he wants. His pranks and tricks often target the gods,
+              challenging their plans and causing uproars. But it's worth
+              mentioning that Loki isn't just a troublemaker; sometimes, he
+              helps the gods. He assists them in retrieving essential items,
+              finding innovative solutions to problems, and interacting with
+              other mythical beings. However, Loki's actions can lead to severe
+              consequences, and his tricks can sometimes cause trouble,
+              conflict, and even disasters. One of the most famous stories
+              involving Loki is the death of Baldur, Odin and Frigg's beloved
+              son. Baldur had ominous dreams about his death, so Frigg made all
+              things promise not to harm him—except for mistletoe, which she
+              overlooked. Loki took advantage of this and made a weapon from
+              mistletoe. He tricked the blind god Hod into throwing it at Baldur
+              during a game, which led to Baldur's death and caused immense
+              sorrow among the gods and other beings.
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/Component/Vanaheim.jsx
+++ src/Component/Vanaheim.jsx
@@ -1,11 +1,267 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Vanaheim() {
+  const containerStyle = {
+    backgroundColor: "black",
+    color: "white",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  };
+
+  const imageStyle = {
+    width: "50%",
+  };
+
+  const smallerImageStyle = {
+    width: "30%",
+    margin: "10px",
+    cursor: "pointer",
+  };
+
+  const headerStyle = {
+    marginBottom: "20px",
+  };
+
+  const imagesContainerStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const leftImageStyle = {
+    flex: 1,
+    cursor: "pointer",
+  };
+
+  const rightImageStyle = {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const [leftImageTextVisible, setLeftImageTextVisible] = useState(false);
+  const [rightImageTextVisible, setRightImageTextVisible] = useState(false);
+
+  const [image1TextVisible, setImage1TextVisible] = useState(false);
+  const [image2TextVisible, setImage2TextVisible] = useState(false);
+  const [image3TextVisible, setImage3TextVisible] = useState(false);
+
+  const toggleLeftImageText = () => {
+    setLeftImageTextVisible(!leftImageTextVisible);
+    setRightImageTextVisible(false);
+  };
+
+  const toggleRightImageText = () => {
+    setRightImageTextVisible(!rightImageTextVisible);
+    setLeftImageTextVisible(false);
+  };
+
+  const toggleImage1Text = () => {
+    setImage1TextVisible(!image1TextVisible);
+    setImage2TextVisible(false);
+    setImage3TextVisible(false);
+  };
+
+  const toggleImage2Text = () => {
+    setImage2TextVisible(!image2TextVisible);
+    setImage1TextVisible(false);
+    setImage3TextVisible(false);
+  };
+
+  const toggleImage3Text = () => {
+    setImage3TextVisible(!image3TextVisible);
+    setImage1TextVisible(false);
+    setImage2TextVisible(false);
+  };
+
   return (
-    <div>
-      <h1>Welcome to Vanaheim</h1>
-      <p>This is the realm of the Vanir, another group of deities.</p>
-      {/* Add content and functionality specific to Vanaheim here */}
+    <div style={containerStyle}>
+      <div style={headerStyle}>
+        <h1>Welcome to Vanaheim</h1>
+        <p>This is the realm of the Vanir, another group of deities.</p>
+      </div>
+
+      <div style={imagesContainerStyle}>
+        <div style={leftImageStyle} onClick={toggleLeftImageText}>
+          <img
+            src={`${process.env.PUBLIC_URL}/realms/VanaheimBG.png`}
+            alt="Left Image"
+            style={imageStyle}
+          />
+          {leftImageTextVisible && (
+            <p>
+              Vanaheim is renowned as the realm of the Vanir, a group of deities
+              associated with fertility, nature, and prosperity themes. This
+              realm is typically envisioned as a lush and vibrant land
+              characterized by its natural abundance and captivating beauty.
+              Vanaheim serves as a sanctuary for divine beings linked to various
+              aspects of life, including agriculture, wealth, affection, and
+              maritime matters. It is often portrayed as a realm of harmony
+              where the Vanir deities ensure the land's fertility and the
+              prosperity of its inhabitants. While Vanaheim may not occupy the
+              same level of prominence as Asgard in mythological narratives, it
+              holds significant importance in the tales of the gods' origins. A
+              noteworthy episode in its history is the conflict between the
+              Vanir and the Aesir, the primary tribe of Norse gods. This war led
+              to tension, followed by a truce, an exchange of hostages, and the
+              subsequent merging of their divine pantheons. Despite being less
+              prevalent in ancient texts and sagas than in realms like Asgard,
+              Vanaheim is referenced in various sources that provide insights
+              into the nature of this realm and the deities who inhabit it. The
+              realm's significance is underscored by its portrayal as a
+              harmonious domain where the Vanir deities exert their influence
+              over the land's fertility and the well-being of its inhabitants.
+              Vanaheim's essence starkly contrasts Asgard's celestial and
+              warrior-focused disposition, emphasizing the gods' deep connection
+              with the natural world. The conflict with the Aesir, the
+              subsequent peace agreement, and the merging of their divine
+              pantheons emphasize the crucial role of the Vanir within the Norse
+              mythological framework. Vanaheim's portrayal as a realm of
+              abundance and prosperity also highlights the significant influence
+              of the Vanir gods in matters related to fertility and wealth,
+              enriching the complexity of Norse mythology. Ultimately, the
+              understated yet vital nature of Vanaheim, coupled with the
+              intriguing character of the Vanir, adds depth and allure to the
+              Norse cosmological landscape.
+            </p>
+          )}
+        </div>
+        <div style={rightImageStyle} onClick={toggleRightImageText}>
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Freyja.png`}
+            alt="Image 1"
+            style={smallerImageStyle}
+            onClick={toggleImage1Text}
+          />
+          {image1TextVisible && (
+            <p>
+              Freyja, a goddess of immense beauty, power, and versatility, is
+              central to the Vanir, a primary group of deities. She embodies
+              love, aesthetic charm, fertility, and mystical abilities. Freyja's
+              image as a captivating and irresistible goddess is a frequent
+              theme in Norse stories. As a fertility goddess, she governs human
+              procreation and agricultural abundance. She embodies love, desire,
+              and sensuality, often summoned in romance and passionate love.
+              Freyja is known for possessing the magical necklace Brísingamen,
+              believed to enhance her beauty and influence. She also shares
+              dominion over the fallen in battle, with half of the warriors
+              claimed for her realm, Folkvangr, while the other half goes to
+              Valhalla under Odin's rule. This dual role reflects the
+              multifaceted nature of Norse deities, representing both
+              life-giving and protective aspects. Freyja's talents extend to
+              magical abilities, particularly in seidr, a unique form of Norse
+              sorcery connected to prophecy and transformation. She is regarded
+              as a master of this mystical craft, even teaching it to other
+              deities, including Odin. Freyja's influence also extends to
+              matters of wealth and prosperity. Her association with gold and
+              precious objects highlights her role as a goddess of abundance.
+              Freyja's striking appeal and captivating charm earn admiration
+              from both divine and mortal beings. Freyja's influence isn't
+              confined to the physical realm, as she possesses significant
+              magical abilities, particularly the mystical art of seidr, which
+              she is credited with teaching other gods, including Odin. Through
+              Freyja, we gain insight into the complex nature of the divine
+              feminine in Norse mythology, where contrasting qualities like
+              gentleness and power, love and war, and nurturing and leadership
+              coexist. Her enduring appeal and multifaceted attributes continue
+              to captivate and inspire, shedding light on the significant role
+              of female deities in Norse myth.
+            </p>
+          )}
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Freyr.png`}
+            alt="Image 2"
+            style={smallerImageStyle}
+            onClick={toggleImage2Text}
+          />
+          {image2TextVisible && (
+            <p>
+              Freyr is known for his association with fertility, wealth, and
+              nature's blessings. He belongs to the Vanir gods, a group closely
+              tied to abundance and prosperity. The Vanir were once in conflict
+              with the Aesir gods before forming an alliance, and Freyr is the
+              son of the sea god Njord, sharing a twin bond with Freyja, the
+              goddess of love and allure. Freyr is often depicted as a handsome
+              and gentle deity, representing our world's nurturing and
+              life-giving aspects. He symbolizes fertility, overseeing
+              agriculture, cultivation, and crop growth. His role is closely
+              connected to the harvest season and the changing seasons, with
+              people venerating him for rich yields and secure livelihoods. One
+              notable story about Freyr involves obtaining the magical ship
+              Skíðblaðnir from the dwarves. This ship has a unique ability to
+              travel on land and sea, symbolizing Freyr's prosperity and ability
+              to bring abundance wherever he goes. Freyr's mythology also
+              includes themes of love and fertility, as he is considered a god
+              of romantic love, marriage, and sensuality. Freyr's friendliness
+              is contrasted by his role in the events leading up to Ragnarok,
+              the apocalyptic battle in Norse mythology. Prophecies indicate
+              that Freyr will engage in mortal combat with the fire giant Surt
+              during this cataclysmic event, leading to his eventual demise.
+              Freyr is a central deity in Norse mythology, representing
+              fertility, wealth, and leadership. His name reflects his role as a
+              ruler and benefactor, symbolizing his authority and generosity. He
+              is associated with agricultural prosperity and is seen as a
+              nurturing god who ensures the Earth's bounty. His blessings extend
+              to happiness, peace, and contentment, highlighting his importance
+              in the Norse worldview, where nature's forces and divine
+              intervention are intricately interconnected. Freyr's character
+              provides insight into the Norse people's reverence for nature's
+              vital provisions and harmonious relationship with the natural
+              world.
+            </p>
+          )}
+          <img
+            src={`${process.env.PUBLIC_URL}/beings/Njord.png`}
+            alt="Image 3"
+            style={smallerImageStyle}
+            onClick={toggleImage3Text}
+          />
+          {image3TextVisible && (
+            <p>
+              Njord is celebrated for his connections to the sea, maritime
+              activities, and wealth. He belongs to the Vanir deities, a group
+              in Norse mythology closely associated with natural elements and
+              abundance. Njord's domain includes the sea, wind, and prosperity,
+              making him a crucial deity for sailors, fishermen, and traders. He
+              is described as a master mariner who can calm storms and ensure
+              safe voyages for those who invoke his name. Njord's influence
+              extends to wealth, representing the treasures provided by the sea,
+              and he also has the power to bestow fertility upon the land,
+              ensuring bountiful harvests. Therefore, Njord is both a god of the
+              sea and a deity of wealth, earning the deep respect of mariners
+              and traders who sought his blessings for successful journeys and
+              prosperity in their endeavors. His connection to fertility also
+              made him the protector of agricultural abundance and productive
+              harvests. Njord's presence in Norse mythology highlights the
+              crucial role of the sea and nature in the lives of the Norse
+              people, underscoring their dependence on these elements for their
+              survival and prosperity. One notable story involving Njord
+              revolves around his marriage to Skadi, a giantess known for her
+              hunting skills. The union between the sea god and the
+              mountain-dwelling Skadi was arranged to promote peace between the
+              divine beings and giants. However, they encountered difficulties
+              due to their differing backgrounds and preferences. Their attempt
+              to divide their time between the mountains and the coast failed to
+              bring them happiness. Eventually, they decided to separate but did
+              so amicably, without animosity. Njord and Skadi continued to
+              respect each other's choices, maintaining a cordial relationship
+              despite parting ways. Njord is a prominent deity in Norse
+              mythology, associated with the sea, wealth, and fertility. His
+              name reflects themes of wealth and generosity. As a Vanir god, he
+              embodies the Norse emphasis on nature, commerce, and prosperity.
+              Njord was highly regarded by sailors and fishermen, and his
+              influence extended to wealth and fertility. His story with Skadi
+              teaches the value of mutual respect and understanding, even in the
+              face of differences. Njord represents more than just a sea god; he
+              symbolizes wealth, fertility, compromise, and mutual respect in
+              Norse mythology.
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
